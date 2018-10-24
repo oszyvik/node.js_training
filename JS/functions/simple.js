312f584c576666e73388ee7778a482ad2a4c169f
oszyvik <oszyvik@gmail.com>
2018-10-24 14:39:44 +0200

    Update JS/functions/simple.js
    array - object hybrid added

--- JS/functions/simple.js
+++ JS/functions/simple.js
@@ -18,14 +18,25 @@ var mapped = nums.map(function(el) {
     return Math.pow(el, 3);
 });
 console.log(mapped);
-
+var getAge = function(){
+    return this.age;
+}
 //Method.
 var user = {
+    __proto__ : Array.prototype,
     name : 'Jolan',
     age: 33,
     greet: function(){
         return 'Szia, ' + this.name + ' vagyok!'
-    }
+    },
+    ga : getAge,
+    0: true,
+    1: 'tr360',
+    length: 2
 }
-
+console.log(user.length);
+for (let index = 0; index < user.length; index++) {
+    console.log(user[index]);    
+}
 console.log(user.greet());
+console.log(user.ga());
