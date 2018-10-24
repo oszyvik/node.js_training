2901b34e8f6bf7495e956af80a09211e08582498
oszyvik <oszyvik@gmail.com>
2018-10-24 11:02:38 +0200

    Update JS/variables/primitives.js
    init repo

---
+++ JS/variables/primitives.js
@@ -0,0 +1,22 @@
+console.log(typeof something);
+//Number.
+var age = 22;
+console.log(typeof age);
+
+
+//Boolean.
+var check = true;
+
+
+//String.
+var name = 'Joe';
+console.log(name.indexOf('Jo'));
+
+{
+//Undefined.
+let something;
+console.log(typeof something);
+}
+
+var senki = null;
+console.log(senki == null);
