625e2fb7fd99affdf6f775d10735271154e63703
oszyvik <oszyvik@gmail.com>
2018-10-24 14:28:53 +0200

    Update JS/functions/simple.js
    object method added

--- JS/functions/simple.js
+++ JS/functions/simple.js
@@ -8,7 +8,24 @@ var multiplier = function (base, m){
 }
 
 var nums = [1,2,3];
-var filtered = nums.filter(function(el){
+var filterFunction = function(el){
     return el % 2 == 0;
-});
+}
+var filtered = nums.filter(filterFunction);
 console.log(filtered);
+
+var mapped = nums.map(function(el) {
+    return Math.pow(el, 3);
+});
+console.log(mapped);
+
+//Method.
+var user = {
+    name : 'Jolan',
+    age: 33,
+    greet: function(){
+        return 'Szia, ' + this.name + ' vagyok!'
+    }
+}
+
+console.log(user.greet());
