ebce014cbb2b94025cb12f92eaf483c07f708fca
oszyvik <oszyvik@gmail.com>
2018-10-25 15:44:39 +0200

    Update NodeJS/basic/helloServer.js
    Changed backticks to singlequotes.

--- NodeJS/basic/helloServer.js
+++ NodeJS/basic/helloServer.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) =>{
     console.log(`Method: ${request.method}, Url: ${request.url}`);
-    response.end(`<h1>I am NodeJS.</h1>`);
+    response.end('<h1>I am NodeJS.</h1>');
 });
 
 server.listen(3333, () =>{
