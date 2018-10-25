40fc18c22cfa47ea114317c5033db06d4c1ec016
oszyvik <oszyvik@gmail.com>
2018-10-25 10:25:27 +0200

    Update JS/practices/basic.js
    Exercise 1.

---
+++ JS/practices/basic.js
@@ -0,0 +1,24 @@
+/**
+ * 1. Print out to the console the current time and date in
+ * the following format:
+ * Today is: Tuesday
+ * Current time is : 10 PM : 30 : 38
+ */
+const currentDate = new Date();
+console.log(`Today is:  ${formatDay()}
+Current time is: ${formatTime()}`);
+
+function formatTime() {
+    let hours = currentDate.getHours() >= 12 ? currentDate.getHours()-12 : currentDate.getHours();
+    let minutes = currentDate.getMinutes();
+    let seconds = currentDate.getSeconds();
+    let amOrPm = hours <= 12 ? 'AM' : 'PM';
+    return `${hours} : ${amOrPm} : ${minutes} : ${seconds}`;
+}
+
+function formatDay() {
+    let days = ['Sunday', 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    let day = currentDate.getDay();
+    return days[day];
+}
+
