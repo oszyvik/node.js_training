e65866e93c5540e7a9005aae7696143c283d43de
oszyvik <oszyvik@gmail.com>
2018-10-25 16:32:51 +0200

    User + Index
    html's added

--- NodeJS/basic/helloServer.js
+++ NodeJS/basic/helloServer.js
@@ -1,11 +1,40 @@
 //Load modules.
-const http = require('http');
+const http = require('http'),
+    path = require('path'),
+    fs = require('fs');
 
-const server = http.createServer((request, response) =>{
+const server = http.createServer((request, response) => {
     console.log(`Method: ${request.method}, Url: ${request.url}`);
-    response.end('<h1>I am NodeJS.</h1>');
+    router(request, response);
 });
 
-server.listen(3333, () =>{
+const router = (req, res) => {
+    let filePath = path.join(__dirname, 'template');
+    if (req.url == '/' || req.url == 'index') {
+        filePath = path.join(filePath, 'index.html');
+    } else {
+        filePath = path.join(filePath, req.url + '.html');
+    }
+
+    readHTML(filePath, (err, data) => {
+        if (err) {
+            res.end('Error in request.');
+            return;
+        }
+        res.end(data);
+    });
+};
+
+const readHTML = (filePath, callBack) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`File not found: ${filePath}`);
+            return callBack(err);
+        }
+        callBack(null, data);
+    });
+};
+
+server.listen(3333, () => {
     console.log('Server is running...');
 });
