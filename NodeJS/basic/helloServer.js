37569c93739d7c8fa9b4af3934d07055877743dd
oszyvik <oszyvik@gmail.com>
2018-10-25 15:44:02 +0200

    Update NodeJS/basic/helloServer.js
    Removed unnecessary lines.

--- NodeJS/basic/helloServer.js
+++ NodeJS/basic/helloServer.js
@@ -3,20 +3,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) =>{
     console.log(`Method: ${request.method}, Url: ${request.url}`);
-    response.end(`<HTML>
-    <head>
-    <title>CSICSKA</title>
-    <blink>I are winner</blink>
-    </head>
-    <body>
-    <div align="center">
-    <marquee behavior="alternate" scrollamount="1">Much CSICSKA...</marquee>
-    <marquee behavior="alternate" scrollamount="12">So JS...</marquee>
-    <marquee behavior="alternate" scrollamount="25">KOLBASZ PROPELLER...</marquee>
-    <marquee behavior="alternate" scrollamount="55">VEGE</marquee>
-    </div>
-    </body>
-    </html>`);
+    response.end(`<h1>I am NodeJS.</h1>`);
 });
 
 server.listen(3333, () =>{
