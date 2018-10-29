a35c7c9888138fde99568b8a30213d30d53cf369
oszyvik <oszyvik@gmail.com>
2018-10-29 11:07:26 +0100

    delete users added
    delete user functionality (partially works)

--- crud/public/javascripts/user.js
+++ crud/public/javascripts/user.js
@@ -1,6 +1,4 @@
 //Get users.
-
-
 let fetchUsers = () => {
     fetch('/users/api/user')
         .then(response => {
@@ -13,6 +11,7 @@ let fetchUsers = () => {
         });
 };
 
+//Send user data to the server
 let postUser = (user) => {
     fetch('users/api/user/' + user.id, {
         method: 'POST',
@@ -20,6 +19,35 @@ let postUser = (user) => {
             'Content-Type': 'application/json; charset=utf-8',
         },
         body: JSON.stringify(user)
+    })
+        .then(response => {
+            return response.json();
+        });
+};
+
+//Send user data to the server
+let putUser = (user) => {
+    fetch('users/api/user/' + user.id, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+        },
+        body: JSON.stringify(user)
+    })
+        .then(response => {
+            return response.json();
+        }).then(json => {
+            console.log(json);
+           // fillTable(json);
+        }).catch(err => {
+            console.log(err);
+        });
+};
+
+//Send user data to the server
+let deleteUser = (user) => {
+    fetch('users/api/user/' + user.id, {
+        method: 'DELETE'
     })
         .then(response => {
             return response.json();
@@ -34,17 +62,26 @@ let tableRow = (user) => {
     let output = `<td>${user.id}</td>`;
     output += `<td>${user.name}</td>`;
     output += `<td>${user.email}</td>`;
-    output += `<button class="send-btn">Send</button>`;
+    output += `<button class="send-btn btn btn-success">Send</button> 
+    <button class="delete-btn btn btn-danger">Delete</button>`;
     let tr = document.createElement('tr');
     tr.innerHTML = output;
-    let button = tr.querySelector('button');
-    button.user = user;
-    button.addEventListener('click', () => {
-        postUser(this.user);
+    let buttonSend = tr.querySelector('.send-btn');
+    buttonSend.user = user;
+    buttonSend.addEventListener('click', () => {
+        postUser(user);
     });
+    let buttonDelete = tr.querySelector('.delete-btn');
+    buttonDelete.user = user;
+    buttonDelete.addEventListener('click', () => {
+        deleteUser(user);
+    });
+
+
+    
     return tr;
-}
-    ;
+};
+
 let fillTable = (json) => {
     let tbody = document.querySelector('tbody');
     tbody.innerHTML = '';
--- crud/routes/users.js
+++ crud/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const path = require('path');
+const fs = require('fs');
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -12,10 +13,51 @@ router.get('/api/user', (req, res, next) => {
   res.sendFile(jsonPath);
 });
 
-router.post('/api/user:id', (req, res, next) => {
+router.post('/api/user/:id', (req, res, next) => {
   console.log(req.params.id);
   console.log(req.body);
-  res.send('OK');
+  res.json({ message: 'ok' });
 });
+
+router.delete('/api/user/:id', (req, res, next) => {
+  console.log(req.params.id);
+  deleteUser(req.params.id);
+  res.json({ message: 'ok' });
+});
+
+router.put('/api/user/:id', (req, res, next) => {
+  res.json({ message: 'ok' });
+});
+
+let deleteUser = async (userId) => {
+  let jsonPath = path.join(__dirname, '../json/users.json');
+  let jsonFile = await readJson(jsonPath);
+  let json = JSON.parse(jsonFile);
+  json.splice(userId-1, 1);
+  let newJson = JSON.stringify(json, null,2);
+  await writeJson(jsonPath, newJson);
+}
+
+const readJson = (filePath, encode = 'utf8') => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, encode, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(data);
+    });
+  });
+};
+
+const writeJson = (filePath, encode = 'utf8', data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filePath, encode, data, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(data);
+    });
+  });
+};
 
 module.exports = router;
